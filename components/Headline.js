6b68cc3455438f3f93bfb1f65245e74e853beacc
Miho Jensen <homiii116@gmail.com>
2024-09-25 15:43:45 +0200

    ♻️ Made a compoment for Headline

---
+++ components/Headline.js
@@ -0,0 +1,20 @@
+
+export function Headline(props) {
+
+  return (
+    <div>
+        <h1 className="text-transform: capitalize">{props.page} Page</h1>
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              pages/{props.page}.js
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import { Footer } from "../components/Footer";
 import { Links } from "@/components/Links";
+import { Headline } from "@/components/Headline";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,17 +29,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1>Index Page</h1>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <Headline page="index" />
 
         <Links />
       </main>
