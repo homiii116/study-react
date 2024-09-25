a71c8787704a265ae11e39ba0dd3ccd83968e082
Miho Jensen <homiii116@gmail.com>
2024-09-25 16:28:14 +0200

    ♻️ Made a component for Main

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import localFont from "next/font/local";
 import { Footer } from "@/components/Footer";
-import { Links } from "@/components/Links";
-import { Headline } from "@/components/Headline";
+import { Main } from "@/components/Main";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,20 +18,7 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Headline page="about" />
-
-        <Links />
-      </main>
-      
+      <Main page="about"/>
       <Footer />
 
     </div>
